6a6935d2d6694943d2b18268f32a8ecf656ed57f
kimjeongbae <gprhf651@naver.com>
2024-02-16 15:40:46 +0900

    회원기능 수정중

--- src/main/java/com/example/Ecommerce/domain/siteuser/Controller/SiteuserContoller.java
+++ src/main/java/com/example/Ecommerce/domain/siteuser/Controller/SiteuserContoller.java
@@ -2,6 +2,7 @@ package com.example.Ecommerce.domain.siteuser.Controller;
 
 import com.example.Ecommerce.domain.siteuser.Entity.SiteuserForm;
 import com.example.Ecommerce.domain.siteuser.Service.SiteuserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,7 +25,7 @@ public class SiteuserContoller {
         return "login_form";
     }
     @GetMapping("/signup")
-    public String SiteuserSignup(Model model) {
+    public String SiteuserSignup(Model model, SiteuserForm siteuserForm) {
 
         return "signup_form";
     }
@@ -32,10 +33,9 @@ public class SiteuserContoller {
     @PostMapping("/signup")
     public String siteuserSignup(@Valid SiteuserForm siteuserForm,
                                BindingResult bindingResult, Model model) {
-
         this.siteuserService.create(siteuserForm.getUserid(), siteuserForm.getPassword1(),
-                siteuserForm.getNickname(), siteuserForm.getEmail());
-        return "redirect:/member/login";
+                siteuserForm.getPassword2(),siteuserForm.getNickname(),siteuserForm.getRole(), siteuserForm.getEmail());
+        return "redirect:/user/login";
     }
 
 }
---
+++ src/main/java/com/example/Ecommerce/domain/siteuser/Entity/SiteRole.java
@@ -0,0 +1,4 @@
+package com.example.Ecommerce.domain.siteuser.Entity;
+
+public class SiteRole {
+}
